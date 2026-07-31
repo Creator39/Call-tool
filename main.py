38b812a3adb8795097d25c60cbf01f3b549301d8
derandri <derandri@student.42antananarivo.mg>
2026-07-31 10:48:16 +0300

    a some function for extraction of function definition

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from llm_sdk import Small_LLM_Model
+from src.functiondef import FunctionDefinition
+from src.functiondef import FunctionCatalog
 import json
 
-def mask_logits(id_autorized: list[int], logits: list[float]) -> list[float]:
+def mask_logits(id_autorized: set[int], logits: list[float]) -> list[float]:
     new_logits = [logits[i] if i in id_autorized else float('-inf') for i in range(len(logits))]
     return new_logits
 
 
+def select_next_token(logits: list[float]) -> int:
+    best : int = logits.index(max(logits))
+    return best
+
+def take_functions_definition(path: str) -> dict:
+    with open(path, "r", encoding='utf-8') as f:
+        load = json.load(f)
+    return load
+
+def load_function_catalog(path: str) -> FunctionCatalog:
+    functions_definition = take_functions_definition(path)
+    function_catalog = FunctionCatalog.model_validate({ "functions": functions_definition })
+    return function_catalog
+
 def main():
-    model = Small_LLM_Model()
-    text = "What is the sum of 2 and 3?"
-    liste = model.encode(text).tolist()[0]
-
-    logist = model.get_logits_from_input_ids(liste)
-
-    best = logist.index(max(logist))
-
-    print(f"le logits le plus elever {best}")
-    vocab_path = model.get_path_to_vocab_file()
-
-    with open(vocab_path,'r', encoding='utf-8') as f:
-        load_file = json.load(f)
-
-    dico_inverse = {j:i for i, j in load_file.items()}
-    candidat = [i for i in range(len(dico_inverse)) if '{' == dico_inverse[i]]
-    new_logits = mask_logits(candidat,logist)
-    best_match = new_logits.index(max(new_logits))
-    print(f"Le meilleur choix {best_match} le texte: {model.decode([best_match])}")
-
+    load = load_function_catalog("data/input/functions_definition.json")
+    for i in load.functions:
+        print(i)
 if __name__ == "__main__":
     main()
